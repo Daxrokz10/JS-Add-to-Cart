50ed0a7951a5a571b7b1003b1727026563f2b126
Daxrokz10 <dakshgagnani@gmail.com>
2025-07-06 12:37:51 +0530

    add cart shi

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 let products = [];
+let cart = [];
 let product_grid = document.querySelector('.tab-content .product-grid')
+let cartSection = document.querySelector('.offcanvas .offcanvas-body .list-group')
 
 async function getProducts() {
     let res = await fetch('js/products.json');
@@ -43,12 +45,58 @@ let displayProducts = (() => {
                                   </button>
                               </span>
                           </div>
-                          <a href="#" class="nav-link">Add to Cart <iconify-icon icon="uil:shopping-cart"></a>
+                          <button class="nav-link add-to-cart-btn" data-id="${id}">Add to Cart <iconify-icon icon="uil:shopping-cart"></button>
                         </div>
                       </div>
         `
         product_grid.appendChild(col);
     });
+
+    document.querySelectorAll('.add-to-cart-btn').forEach(btn => {
+      btn.addEventListener('click', function() {
+        const prodId = parseInt(this.getAttribute('data-id'));
+        let cart = JSON.parse(localStorage.getItem('cart')) || [];
+        let prod = products.find(p => p.id === prodId);
+        let currentName = prod.name;
+
+        let found = cart.find(item => item.id === prodId);
+        if (found) {
+          found.qty += 1;
+        } else {
+          cart.push({
+            id: prod.id,
+            name: prod.name,
+            price: prod.price,
+            image: prod.image,
+            qty: 1
+          });
+        }
+        localStorage.setItem('cart', JSON.stringify(cart));
+        renderCart();
+        alert(`Added ${currentName} to cart`);
+      });
+    })
+    renderCart();
 });
+
+let renderCart = () => {
+  // Clear previous cart items
+  cartSection.innerHTML = '';
+  // Get latest cart from localStorage
+  let cart = JSON.parse(localStorage.getItem('cart')) || [];
+  cart.forEach(item => {
+    let { price, name, qty } = item;
+    let li = document.createElement('li');
+    li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'lh-sm');
+    li.innerHTML = `
+      <div>
+        <h6 class="my-0">${name} <span class="badge bg-secondary">${qty}</span></h6>
+        <small class="text-body-secondary">x${qty}</small>
+      </div>
+      <span class="text-body-secondary">$${(price * qty).toFixed(2)}</span>
+    `;
+    cartSection.appendChild(li);
+  });
+};
 
 getProducts();
